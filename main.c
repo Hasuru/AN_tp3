49a86e578317df0d96fa61c980671e385160d43e
Hasuru <hugo.as.silva25@gmail.com>
2022-11-25 11:45:16 +0000

    metodo de newton added [untested]

--- main.c
+++ main.c
@@ -4,35 +4,92 @@
 #define LEN 7
 
 double xn[LEN+1] = {-1, -0.714, -0.428, -0.142, 0.144, 0.430, 0.716, 1};
+double m[LEN+1][LEN+1];
 
-double lagrange(double x);
-double l(double x, int i);
-double f(double x);
+void clearMatrix();
+void printMatrix();
+
+double newton(double x);
+double h(double x, int i);
+void dividedDiff();
+double f(double x); // funciona direito (feito com radianos)
 
 int main() {
+    clearMatrix();
+    dividedDiff();
+
+    double val1 = newton(0.1);
+    double val2 = newton(0.9);
+
+    /* dividedDiff Testes -> ainda por avaliar
+    int parcelas = LEN;
+    for (int i = 0; i <= LEN; i++) {
+        for (int j = 0; j <= parcelas; j++) {
+            printf("%lf\t", m[i][j]);
+        }
+        --parcelas;
+        putchar('\n');
+    }*/
+
+    /* funcao h Testes -> Funcional
+    double testVal = 1;
+    for (int i = 0; i <= LEN; i++) {
+        testVal *= h(0.1,i);
+        printf("h(0.1, %d) -> %.3f\n", i, h(0.1, i));
+        printf("testVal -> %lf\n-=-\n", testVal);
+    }*/
+
     return 0;
 }
 
-double f(double x) {
-    return pow(x,2) + sin(6*x);
-}
-
-double l(double x, int i) {
-    double num = 1, denom = 1;
-    for (int j = 0; j < LEN; j++) {
-        if (j != i) {
-            num *= x - xn[j];
-            denom *= xn[i] - xn[j];
+void clearMatrix() {
+    for (int i = 0; i <= LEN; i++) {
+        for (int j = 0; j <= LEN; j++) {
+            m[i][j] = 0;
         }
     }
-
-    return num / denom;
 }
 
-double lagrange(double x) {
+void printMatrix() {
+    for (int i = 0; i <= LEN; i++) {
+        for (int j = 0; j <= LEN; j++) {
+            printf("%lf\t", m[i][j]);
+        }
+        putchar('\n');
+    }
+}
+
+// function values
+double f(double x) {
+    return x*x + sin(6*x);
+}
+
+// f(xi) -> f[x0, ... ,xn]
+void dividedDiff() {
+    // 1st row
+    for (int i = 0; i <= LEN; i++) {
+        m[0][i] = f(xn[i]);
+    }
+
+    for (int i = 1; i <= LEN; i++) {
+        for (int j = 0; j <= LEN; j++) {
+            m[i][j] = (m[i-1][j+1] - m[i-1][j]) / (xn[j] - xn[i+j]);
+        }
+    }
+}
+
+// (x - x0)(x - x1)...(x - xn)
+double h(double x, int i) {
+    return x - xn[i];
+}
+
+// f(x0) + h(0)f[x0, x1] + ... + h(n-1)f[x0, ... , xn]
+double newton(double x) {
     double val = 0;
-    for (int i = 0; i < LEN; i++) {
-        val += l(x,i) * f(xn[i]);
+    double hval = 1;
+    for (int i = 0; i <= LEN; i++) {
+        val += hval*m[i][0];
+        hval *= h(x, i);
     }
     return val;
 }
